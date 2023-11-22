3668062b94965fc5c1b9e2e5f474412f7156ffe3
lenilsonb <lenilsonbarbosa@outlook.com>
2023-11-22 12:45:04 -0300

    init commit

---
+++ Aula13/src/conexao/Aula13.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conexao;
+
+import util.conecta;
+
+
+/**
+ *
+ * @author ADS2311011
+ */
+public class Aula13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException {
+        
+        conecta c = new conecta();
+        c.conectar();
+        
+    }
+    
+}
---
+++ Aula13/src/util/conecta.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.*;
+/**
+ *
+ * @author ADS2311011
+ */
+public class conecta {
+    public static Connection conectar() throws ClassNotFoundException{
+        Connection conn = null;
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/fatec","root","");
+        }catch (SQLException e){
+            System.err.println(""+e.getMessage());
+        }
+        
+        return conn;
+    }
+}
